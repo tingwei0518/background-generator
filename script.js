ab08fe056f79b855a4147ae5e8fd983a4dd40c19
tingwei0518 <56964748+tingwei0518@users.noreply.github.com>
2022-03-04 17:34:18 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var body = document.querySelector("body");
-var color1 = document.querySelector("#color1");
-var color2 = document.querySelector("#color2");
-var css = document.querySelector("h3");
-var randomButton = document.querySelector("#random");
+const body = document.querySelector("body");
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+const css = document.querySelector("h3");
+const randomButton = document.querySelector("#random");
 
 function setGradient(){	
-	body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")";
-	css.textContent = body.style.background + ";";
+	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+	css.textContent = `${body.style.background};`;
 }
 
 function setRandomGradient(){
-	var randomColor1 = Math.floor(Math.random()*16777215).toString(16);
-	var randomColor2 = Math.floor(Math.random()*16777215).toString(16);
-	color1.value = "#" + randomColor1;
-	color2.value = "#" + randomColor2;
+	const randomColor1 = Math.floor(Math.random()*16777215).toString(16);
+	const randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+	color1.value = `#${randomColor1}`;
+	color2.value = `#${randomColor2}`;
 	setGradient();
 }
 
