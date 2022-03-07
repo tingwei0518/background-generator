8a2fea82359b9f7b16ce44e4618f1b6a4b431d20
tingwei0518 <56964748+tingwei0518@users.noreply.github.com>
2022-03-07 16:16:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const color2 = document.querySelector("#color2");
 const css = document.querySelector("h3");
 const randomButton = document.querySelector("#random");
 
-function setGradient(){	
+const setGradient = () => {	
 	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
 	css.textContent = `${body.style.background};`;
 }
 
-function setRandomGradient(){
+const setRandomGradient = () => {
 	const randomColor1 = Math.floor(Math.random()*16777215).toString(16);
 	const randomColor2 = Math.floor(Math.random()*16777215).toString(16);
 	color1.value = `#${randomColor1}`;
